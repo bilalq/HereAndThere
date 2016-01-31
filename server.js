48510eeb609fe1125c4ef8bf5ecbdf0cd8aa064b
Bilal Quadri <bilalquadri92@gmail.com>
2016-01-31 09:30:05 -0800

    Configure API proxy

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+/*eslint no-var: 0, prefer-arrow-callback: 0*/
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.config')
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+  historyApiFallback: true,
+  proxy: {
+    '/tsp|places|atob*': {
+      target: 'http://localhost:3000',
+      secure: false
+    }
+  }
+}).listen(3001, 'localhost', function (err, result) {
+  console.log(result)
   if (err) {
-    console.log(err);
+    console.log(err)
   }
 
-  console.log('Listening at localhost:3000');
-});
+  console.log('Listening at localhost:3001')
+})
